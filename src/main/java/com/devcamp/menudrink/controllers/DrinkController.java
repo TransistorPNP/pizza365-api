a76a099beca6a2da6c487fa4025d783e3ed459dd
Vi Ngoc Hoang <hoangvn@devcamp.edu.vn>
2022-11-10 17:55:54 +0700

    full drink response

--- src/main/java/com/devcamp/menudrink/controllers/DrinkController.java
+++ src/main/java/com/devcamp/menudrink/controllers/DrinkController.java
@@ -52,4 +52,42 @@ public class DrinkController {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    @PutMapping("/drinks/{id}")
+    public ResponseEntity<Object> updateDrinkById(@Valid @RequestBody Drink newDrink, @PathVariable Integer id) {
+        try {
+            Optional<Drink> optional = drinkRepository.findById(id);
+            if (!optional.isPresent()) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+            Drink thisDrink = optional.get();
+
+            thisDrink.setMaNuocUong(newDrink.getMaNuocUong());
+            thisDrink.setTenNuocUong(newDrink.getTenNuocUong());
+            if (newDrink.getDonGia() != 0) {
+                thisDrink.setDonGia(newDrink.getDonGia());
+            }
+            thisDrink.setNgayCapNhat(new Date());
+            Drink saved = drinkRepository.save(thisDrink);
+            return new ResponseEntity<>(saved, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/drinks/{id}")
+    public ResponseEntity<Object> deleteDrinkById(@PathVariable Integer id) {
+        try {
+            Optional<Drink> optional = drinkRepository.findById(id);
+            if (optional.isPresent()) {
+                drinkRepository.deleteById(id);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return ResponseEntity.unprocessableEntity().body("Không tồn tại nước uống với ID này!");
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
